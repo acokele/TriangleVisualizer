436b2c548442fb782db847b98d7cd2850042f97e
acokele <acokele@gmail.com>
2015-02-21 11:38:38 +0100

    Added events to triangle point

--- TriangleVisualizer/Triangle.cs
+++ TriangleVisualizer/Triangle.cs
@@ -6,21 +6,101 @@ using System.Drawing;
 
 namespace TriangleVisualizer
 {
+    public class CoordinateChangedEventArgs : EventArgs
+    {
+        private float _oldValue;
+        private float _newValue;
+
+        public float OldValue { get { return _oldValue; } private set { _oldValue = value; } }
+        public float NewValue { get { return _newValue; } private set { _newValue = value; } }
+
+        public CoordinateChangedEventArgs(float oldValue, float newValue)
+        {
+            OldValue = oldValue; NewValue = newValue;
+        }
+
+    }
+
+    public delegate void CoordinateChangedEventHandler(object sender, CoordinateChangedEventArgs e);
+
+
     public class TrianglePoint
     {
-        public float X { get; set; }
-        public float Y { get; set; }
 
+        private float _x;
+        private float _y;
+
+        public event CoordinateChangedEventHandler XCoordinateChanged;
+        public event CoordinateChangedEventHandler YCoordinateChanged;
+
+        protected virtual void OnXCoordinateChanged(CoordinateChangedEventArgs e)
+        {
+            if (XCoordinateChanged != null)
+                XCoordinateChanged(this, e);
+        }
+
+        protected virtual void OnYCoordinateChanged(CoordinateChangedEventArgs e)
+        {
+            if (YCoordinateChanged != null)
+                YCoordinateChanged(this, e);
+        }
+
+        /// <summary>
+        /// Gets or sets the X coordinate of the point.
+        /// </summary>
+        public float X {
+            get
+            { 
+                return _x;  
+            }
+            set
+            {
+                float oldValue = _x;
+                _x = value;
+
+                OnXCoordinateChanged(new CoordinateChangedEventArgs(oldValue, value));
+            }
+        }
+        /// <summary>
+        /// Gets or sets the Y coordinate of the point.
+        /// </summary>
+        public float Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                float oldValue = _y;
+                _y = value;
+
+                OnYCoordinateChanged(new CoordinateChangedEventArgs(oldValue, value));
+            }
+        }
+
+        /// <summary>
+        /// Point with coordinates (0, 0).
+        /// </summary>
         public readonly static TrianglePoint Zero = new TrianglePoint();
 
+        /// <summary>
+        /// Creates a new point with specified coordinates.
+        /// </summary>
+        /// <param name="x">The point's X coordinate.</param>
+        /// <param name="y">The point's Y coordinate.</param>
         public TrianglePoint(float x, float y)
         {
             X = x; Y = y;
         }
 
+        /// <summary>
+        /// Creates a new point with coordinates (0, 0)
+        /// </summary>
         public TrianglePoint() : this(0, 0)
         {}
 
+        #region Operators
         public static TrianglePoint operator+(TrianglePoint a, TrianglePoint b)
         {
             return new TrianglePoint(a.X + b.X, a.Y + b.Y);
@@ -41,14 +121,16 @@ namespace TriangleVisualizer
             return new TrianglePoint(a * p.X, a * p.Y);
         }
 
-        public static implicit operator PointF(TrianglePoint p)
-        {
-            return new PointF(p.X, p.Y);
-        }
-
         public static TrianglePoint operator/(TrianglePoint p, float a)
         {
             return new TrianglePoint(p.X / a, p.Y / a);
+        }
+
+        #endregion
+
+        public static implicit operator PointF(TrianglePoint p)
+        {
+            return new PointF(p.X, p.Y);
         }
 
         public static implicit operator TrianglePoint(PointF p)
@@ -59,20 +141,64 @@ namespace TriangleVisualizer
 
     public class Triangle
     {
+        /// <summary>
+        /// Coordinates of point A.
+        /// </summary>
         public TrianglePoint A { get; set; }
-        public TrianglePoint B { get; set; }
-        public TrianglePoint C { get; set; }
 
+        /// <summary>
+        /// Coordinates of point B.
+        /// </summary>
+        public TrianglePoint B { get; private set; }
+
+        /// <summary>
+        /// Coordinates of point C.
+        /// </summary>
+        public TrianglePoint C { get; private set; }
+
+        /// <summary>
+        /// Length of side BC.
+        /// </summary>
         public float SideA { get; private set; }
+
+        /// <summary>
+        /// Length of side AC.
+        /// </summary>
         public float SideB { get; private set; }
+
+        /// <summary>
+        /// Length of side AB.
+        /// </summary>
         public float SideC { get; private set; }
 
+        /// <summary>
+        /// Angle BAC (in radians).
+        /// </summary>
         public float Alpha { get; private set; }
+
+        /// <summary>
+        /// Angle ABC (in radians).
+        /// </summary>
         public float Beta { get; private set; }
+
+        /// <summary>
+        /// Angle ACB (in radians).
+        /// </summary>
         public float Gamma { get; private set; }
 
+        /// <summary>
+        /// Area of the triangle.
+        /// </summary>
         public float Area { get; private set; }
+
+        /// <summary>
+        /// Perimeter of the triangle.
+        /// </summary>
         public float Perimeter { get; private set; }
+
+        /// <summary>
+        /// False if points A, B and C are colinear, true otherwise/
+        /// </summary>
         public bool IsTriangle { get; private set; }
         
         public Triangle(TrianglePoint _a, TrianglePoint _b, TrianglePoint _c)
